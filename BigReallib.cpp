10826c0d325cc53118da8f369a09f7819428b15a
ammarmorad24 <45663746+ammarmorad24@users.noreply.github.com>
2023-10-28 20:54:05 +0200

    Update BigReallib.cpp

--- BigReallib.cpp
+++ BigReallib.cpp
@@ -9,3 +9,39 @@ bool BigReal:: isValidReal(string realNumber) {
     return (regex_match (realNumber, regex("[+-]?\\d*\\.?\\d*")));
 }
 
+BigReal(){
+    BigNum = "0.0";
+}
+
+BigReal(string realNumber){
+    if(isValidReal(realNumber)){
+        BigNum = realNumber;
+    }
+    else{
+        cout << "invalid number";
+    }
+}
+
+BigReal(const BigReal& other){ //Copy Constructor
+    BigNum = other.BigNum;
+}
+
+BigReal &operator=(BigReal &other){ //Assignment Operator
+    BigNum = other.BigNum;
+    return other;
+}
+
+int sign()
+{
+    if(isdigit(BigNum[0]) ||  BigNum[0] == '+' || BigNum[0] == '.')
+    {
+        return 1;  //returns 1 as a positive value
+    }
+    else
+    {
+        return -1; //returns -1 as a negative value
+    }
+}
+
+
+
