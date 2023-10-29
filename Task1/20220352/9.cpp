8cf0e9b3b8aa5c5e1a728a556f515bdcab4459a7
Miguel-Reda <147875415+Miguel-Reda@users.noreply.github.com>
2023-10-29 21:01:00 +0200

    task 9

--- Task1/20220352/9.cpp
+++ Task1/20220352/9.cpp
@@ -7,3 +7,43 @@
 // Date: 28 Oct 2023#include <bits/stdc++.h>
 #include <bits/stdc++.h>
 using namespace std;
+bool bear(int n)
+{
+    if (n == 42)
+    {
+        return true;
+    }
+    else if (n % 5 == 0)
+    {
+        n -= 42;
+        return bear(n);
+    }
+    else if (n % 2 == 0 && n > 83)
+    {
+        n /= 2;
+        return bear(n);
+    }
+    else if (n % 3 == 0 || n % 4 == 0)
+    {
+        n -= ((n % 10) * ((n % 100) / 10));
+        return bear(n);
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (bear(a))
+    {
+        cout << "true";
+    }
+    else
+    {
+        cout << "false";
+    }
+}
