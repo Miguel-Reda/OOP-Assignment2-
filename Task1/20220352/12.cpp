a9eae236970d9aa0eac7f1b220baef234950ec0a
Miguel-Reda <147875415+Miguel-Reda@users.noreply.github.com>
2023-10-29 20:30:41 +0200

    task 6 part a

--- Task1/20220352/12.cpp
+++ Task1/20220352/12.cpp
@@ -1,7 +1,9 @@
-// File: ………cpp
-// Purpose:
-// Author:
+// File: 3.cpp
+// Purpose: The program take a string and separate all the elements and delete spaces and store it in a vector.
+// Author: Miguel Reda
 // Section: S9 Wednesday 2nd slot
-// ID:
+// ID:20220352
 // TA: Samar Sabry Shaker
-// Date: …… Oct 2023
+// Date: 28 Oct 2023#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
--- Task1/20220352/6.cpp
+++ Task1/20220352/6.cpp
@@ -1,7 +1,36 @@
-// File: ………cpp
-// Purpose:
-// Author:
+// File: 3.cpp
+// Purpose: The program take a string and separate all the elements and delete spaces and store it in a vector.
+// Author: Miguel Reda
 // Section: S9 Wednesday 2nd slot
-// ID:
+// ID:20220352
 // TA: Samar Sabry Shaker
-// Date: …… Oct 2023
+// Date: 28 Oct 2023#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+//part a
+void binaryPrint(int n, string s)
+{
+    if (n > 0)
+    {
+        binaryPrint(n/2,s);
+        if (n % 2 == 0)
+        {
+            s.push_back('0');
+        }
+        else
+        {
+            s.push_back('1');
+        }
+        n = floor(n);
+    }
+    cout << s;
+}
+//part b
+
+int main()
+{
+    string s;
+    int n;
+    cin >> n;
+    binaryPrint(n,s );
+}
