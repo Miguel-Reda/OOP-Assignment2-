114894db5dfbbccf4b02085a2e5e321db01b5150
Miguel-Reda <147875415+Miguel-Reda@users.noreply.github.com>
2023-10-29 22:06:57 +0200

    task 12

--- Task1/20220352/12.cpp
+++ Task1/20220352/12.cpp
@@ -7,3 +7,56 @@
 // Date: 28 Oct 2023#include <bits/stdc++.h>
 #include <bits/stdc++.h>
 using namespace std;
+map<string, int> phishingKeywords = {
+    {"password", 3},
+    {"account", 3},
+    {"verify", 3},
+    {"urgent", 3},
+    {"login", 3},
+    {"update", 2},
+    {"bank", 3},
+    {"creditcard", 3},
+    {"social security number", 3},
+    {"security alert", 2},
+    {"paypal", 3},
+    {"financial", 1},
+    {"suspicious activity", 2},
+    {"unauthorized access", 2},
+    {"limited time offer", 1},
+    {"click here", 2},
+    {"phishing", 3},
+    {"lottery", 1},
+    {"congratulations", 1},
+    {"IRS", 2},
+    {"free", 1},
+    {"win", 1},
+    {"claim", 1},
+    {"offer", 1},
+    {"urgent action required", 2},
+    {"prize", 1},
+    {"exclusive deal", 1},
+    {"reward", 1},
+    {"guaranteed", 1},
+    {"secret", 1}};
+int main()
+{
+    int Total_count = 0;
+    string file_name, track;
+    cin >> file_name;
+    ifstream input_file(file_name);
+    map<string, int> found_words;
+    while (input_file >> track)
+    {
+        transform(track.begin(), track.end(), track.begin(), ::tolower);
+        if (phishingKeywords.find(track) != phishingKeywords.end())
+        {
+            found_words[track]++;
+            Total_count += phishingKeywords[track];
+        }
+    }
+    for (auto it = found_words.begin(); it != found_words.end(); it++)
+    {
+        cout << it->first << "  " << it->second << '\n';
+    }
+    cout << "Total count is: " << Total_count;
+}
