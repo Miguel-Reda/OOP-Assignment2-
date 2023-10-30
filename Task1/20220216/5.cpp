2e0296a6ed1e9b5744210e43e20748fd8a4a1264
ammarmorad24 <45663746+ammarmorad24@users.noreply.github.com>
2023-10-30 12:16:49 +0200

    Update 5.cpp

--- Task1/20220216/5.cpp
+++ Task1/20220216/5.cpp
@@ -5,3 +5,59 @@
 // ID:
 // TA: Samar Sabry Shaker
 // Date: …… Oct 2023
+#include <bits/stdc++.h>
+using namespace std;
+pair<string ,int> arr[10];
+
+void AddPlayer(){
+    pair<string ,int> InputPlayer;
+    cin >> InputPlayer.first >> InputPlayer.second;
+    for (int i = 0; i < 10; ++i) {
+        if (InputPlayer.second > arr[i].second) {
+            swap(InputPlayer,arr[i]);
+        }
+    }
+}
+
+void printTopTen(){
+    for (int i = 0; i < 10; ++i) {
+        if (arr[i].second == 0) break;
+        cout << arr[i].first << ": " << arr[i].second << '\n';
+    }
+}
+
+void EnterPlayerName(){
+    string name;
+    cin >> name;
+    for (int i = 0; i < 10; ++i) {
+        if (arr[i].first == name){
+            cout << arr[i].second << '\n';
+            return;
+        }
+    }
+    cout << "Player is not in top 10\n";
+
+}
+
+
+int main() {
+    char Menu[] =
+            "Please select an option to apply or 0 to exit:\n"
+            "1- Add a New Player\n"
+            "2- Print Top Ten Players\n"
+            "3- Enter a Player Name\n"
+            "0- Exit";
+    int option;
+    void (*menu[])() = {AddPlayer,printTopTen,EnterPlayerName};
+
+    while (true) {
+        puts(Menu);
+        cin >> option;
+        if (option == 0) break;
+        menu[option - 1]();
+
+    }
+
+
+
+}
