747df1901c9ca33a5832aad3d52d8924f312ccc8
ammarmorad24 <45663746+ammarmorad24@users.noreply.github.com>
2023-10-30 12:16:18 +0200

    Update 2.cpp

--- Task1/20220216/2.cpp
+++ Task1/20220216/2.cpp
@@ -5,3 +5,26 @@
 // ID:
 // TA: Samar Sabry Shaker
 // Date: …… Oct 2023
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+void inclusive_speech(string& sentence) {
+    // Replace masculine pronouns with both-gender pronouns
+    sentence = regex_replace(sentence, regex("\\b([hH]e)\\b"), "$1 or she");
+    sentence = regex_replace(sentence, regex("\\b([hH]im)\\b"), "$1 or her");
+
+
+
+
+}
+
+int main() {
+    string input_sentence;
+    getline(cin,input_sentence);
+    inclusive_speech(input_sentence);
+    cout << input_sentence << endl;
+}
+
