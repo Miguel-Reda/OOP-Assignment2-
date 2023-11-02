ffa925cb5c0c5b35d17ee541c0adbe8c133b5128
ammarmorad24 <45663746+ammarmorad24@users.noreply.github.com>
2023-11-02 14:17:54 +0200

    Update 5.cpp

--- Task1/20220216/5.cpp
+++ Task1/20220216/5.cpp
@@ -1,8 +1,8 @@
 // File: ………cpp
-// Purpose:
-// Author:
+// Purpose: player list management
+// Author: Ammar Mohamed Mahmoud Hamed
 // Section: S9 Wednesday 2nd slot
-// ID:
+// ID: 20220216
 // TA: Samar Sabry Shaker
 // Date: …… Oct 2023
 #include <bits/stdc++.h>
