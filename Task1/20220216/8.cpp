ad45d289ad2bb54cb3b90ca338d81e7f9191c9e4
ammarmorad24 <45663746+ammarmorad24@users.noreply.github.com>
2023-11-03 22:02:30 +0200

    Update 8.cpp

--- Task1/20220216/8.cpp
+++ Task1/20220216/8.cpp
@@ -1,7 +1,33 @@
 // File: ………cpp
-// Purpose:
-// Author:
+// Purpose: recursive fractal pattern builder
+// Author: Ammar Mohamed Mahmoud Hamed
 // Section: S9 Wednesday 2nd slot
-// ID:
+// ID: 20220216
 // TA: Samar Sabry Shaker
 // Date: …… Oct 2023
+
+#include <iostream>
+
+using namespace std;
+void pattern(int n,int i)
+{
+    if(n==0)
+    {
+        return;
+    }
+    pattern(n/2,i);
+        for(int j=0;j<i;++j)
+        {
+            cout<<"  ";
+        }
+        for(int j=0;j<n;++j)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+        pattern(n/2,i + n/2);
+}
+int main() {
+    pattern(8,0);
+    return 0;
+}
