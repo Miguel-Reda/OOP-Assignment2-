6eec78bb213d164b140424c048217224fd2bf826
ammarmorad24 <45663746+ammarmorad24@users.noreply.github.com>
2023-11-03 22:07:02 +0200

    Update BigReallib.h

--- all_but_not_complete/BigReallib.h
+++ all_but_not_complete/BigReallib.h
@@ -22,17 +22,16 @@ public:
 
     BigReal(double realNumber /* = 0.0 */); // Default constructor
     BigReal(string realNumber = "0.0");     // Initialize from string
-    BigReal(const BigReal &other);          // Copy constructor
+
     BigReal &operator=(const BigReal &other);     // Assignment operator
-    // Depending on how you store data, default can be enough
-    void setNum(string realNumber);
+
+
 
     int size();
 
     int sign();
 
-
-    friend BigReal operator+(BigReal first, BigReal other);
+    BigReal operator+ (BigReal other);
 
     BigReal operator-(BigReal other);
 
