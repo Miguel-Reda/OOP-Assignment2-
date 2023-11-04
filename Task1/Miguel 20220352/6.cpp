59d0982f7a42622cffe424d3a26cc9bf148e9fb4
Miguel-Reda <147875415+Miguel-Reda@users.noreply.github.com>
2023-11-04 21:55:53 +0200

    task 6 b

--- Task1/Miguel 20220352/6.cpp
+++ Task1/Miguel 20220352/6.cpp
@@ -1,4 +1,4 @@
-// File: 3.cpp
+// File: 7.cpp
 // Purpose: The program take a string and separate all the elements and delete spaces and store it in a vector.
 // Author: Miguel Reda
 // Section: S9 Wednesday 2nd slot
@@ -7,12 +7,12 @@
 // Date: 28 Oct 2023
 #include <bits/stdc++.h>
 using namespace std;
-//part a
+// part a
 void binaryPrint(int n, string s)
 {
     if (n > 0)
     {
-        binaryPrint(n/2,s);
+        binaryPrint(n / 2, s);
         if (n % 2 == 0)
         {
             s.push_back('0');
@@ -25,12 +25,25 @@ void binaryPrint(int n, string s)
     }
     cout << s;
 }
-//part b
-
+// part b
+void numbers(string Prefix, int k)
+{
+    if (k == 0)
+    {
+        cout << Prefix<<'\n';
+    }
+    else
+    {
+        numbers(Prefix + "0", k - 1);
+        numbers(Prefix + "1", k - 1);
+    }
+}
 int main()
 {
     string s;
     int n;
+    cin >> s;
     cin >> n;
-    binaryPrint(n,s );
+    numbers(s,n);
+    /* binaryPrint(n, s); */
 }
