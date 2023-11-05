2fb75df5b81d74e10570805c157993f1ac1937cf
Miguel-Reda <147875415+Miguel-Reda@users.noreply.github.com>
2023-11-05 12:12:20 +0200

    done

--- Task1/Miguel 20220352/3.cpp
+++ Task1/Miguel 20220352/3.cpp
@@ -8,20 +8,32 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+// The split function takes two strings as input: 'target' (the string to be split) and 'delimiter' (the separator used to split the string).
+// It splits the 'target' string into multiple substrings based on the 'delimiter'.
+// The function iteratively finds the 'delimiter' in the 'target' string, extracts the substring before the 'delimiter', and adds it to the result vector.
+// It then removes the extracted substring along with the 'delimiter' from the 'target' string and continues the process until no more instances of 'delimiter' are found.
+// Finally, the remaining 'target' string (which does not contain any more delimiters) is added to the result vector.
+// The function returns a vector of strings containing the split substrings.
+
 vector<string> split(string target, string delimiter)
 {
-    vector<string> result;
-    int index = 0;
-    string take;
-    int d = delimiter.size();
+    vector<string> result; // Vector to store the split substrings.
+    int index = 0; // Variable to keep track of the position of the found delimiter in the 'target' string.
+    string take; // Temporary variable to store the extracted substring before the delimiter.
+    int d = delimiter.size(); // Size of the delimiter string.
+
+    // Loop until the delimiter is found in the 'target' string.
     while ((index = target.find(delimiter)) != string::npos)
     {
-        take = target.substr(0, index);
-        result.push_back(take);
-        target.erase(0, index + d);
+        take = target.substr(0, index); // Extract substring before the delimiter.
+        result.push_back(take); // Add the extracted substring to the result vector.
+        target.erase(0, index + d); // Remove the extracted substring along with the delimiter from the 'target' string.
     }
+
+    // Add the remaining 'target' string (without any more delimiters) to the result vector.
     result.push_back(target);
-    return result;
+
+    return result; // Return the vector containing the split substrings.
 }
 int main()
 {
